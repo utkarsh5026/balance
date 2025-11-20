ee5c6fc4dbb0cdee47194ebc88253156013e8916
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-11-20 21:19:29 +0530

    Implement Weighted Round Robin Balancer and remove redundant weight.go file

--- pkg/balance/rr.go
+++ pkg/balance/rr.go
@@ -31,3 +31,52 @@ func (r *RoundRobinBalancer) Select() (*node.Node, error) {
 	selected := healthy[int(index)%len(healthy)]
 	return selected, nil
 }
+
+type WeightedRoundRobinBalancer struct {
+	pool    *node.Pool
+	current atomic.Uint64
+}
+
+func NewWeightedRoundRobinBalancer(pool *node.Pool) *WeightedRoundRobinBalancer {
+	return &WeightedRoundRobinBalancer{
+		pool: pool,
+	}
+}
+
+func (b *WeightedRoundRobinBalancer) Name() LoadBalancerType {
+	return WeightedRoundRobin
+}
+
+func (b *WeightedRoundRobinBalancer) Select() (*node.Node, error) {
+	healthy := b.pool.Healthy()
+	if err := checkHealthy(healthy); err != nil {
+		return nil, err
+	}
+
+	if len(healthy) == 1 {
+		return healthy[0], nil
+	}
+
+	var totalWeight int
+	for _, n := range healthy {
+		totalWeight += n.Weight()
+	}
+
+	if totalWeight <= 0 {
+		next := b.current.Add(1)
+		selected := healthy[int(next)%len(healthy)]
+		return selected, nil
+	}
+
+	next := b.current.Add(1)
+	weightIndex := int(next % uint64(totalWeight))
+
+	for _, n := range healthy {
+		weightIndex -= n.Weight()
+		if weightIndex < 0 {
+			return n, nil
+		}
+	}
+
+	return healthy[0], nil
+}
