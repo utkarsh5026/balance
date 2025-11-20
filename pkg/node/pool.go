1bead5ed461b9255246447f4f8c84b0df35c95a7
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-11-20 18:22:22 +0530

    Implement node pool with methods for adding, removing, and counting nodes

---
+++ pkg/node/pool.go
@@ -0,0 +1,74 @@
+package node
+
+import (
+	"slices"
+	"sync"
+)
+
+type Pool struct {
+	nodes []*Node
+	mu    sync.RWMutex
+}
+
+func NewPool() *Pool {
+	return &Pool{
+		nodes: make([]*Node, 0),
+	}
+}
+
+func (p *Pool) Add(node *Node) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.nodes = append(p.nodes, node)
+}
+
+func (p *Pool) Remove(node *Node) bool {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	for i, n := range p.nodes {
+		if n == node {
+			p.nodes = append(p.nodes[:i], p.nodes[i+1:]...)
+			return true
+		}
+	}
+	return false
+}
+
+func (p *Pool) All() []*Node {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+	return slices.Clone(p.nodes)
+}
+
+func (p *Pool) Healthy() []*Node {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+
+	healthyNodes := make([]*Node, 0, len(p.nodes))
+	for _, n := range p.nodes {
+		if n.IsHealthy() {
+			healthyNodes = append(healthyNodes, n)
+		}
+	}
+
+	return healthyNodes
+}
+
+func (p *Pool) Count() int {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+	return len(p.nodes)
+}
+
+func (p *Pool) HealthyCount() int {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+	count := 0
+	for _, n := range p.nodes {
+		if n.IsHealthy() {
+			count++
+		}
+	}
+
+	return count
+}
